2c83977c38254bdf37f43ce4e6684d742f4d0fa6
Adam Forgac <aforg@email.cz>
2023-01-02 01:59:27 +0100

    Create numerical operations
    Simple numerical operations were created. Every operation has
    three parameters, first number, operator and the second number.

---
+++ script.js
@@ -0,0 +1,44 @@
+// NUMERICAL OPERATIONS
+// NUMERICAL OPERATIONS
+// NUMERICAL OPERATIONS
+
+
+
+// ADD
+
+
+function sum(num1, num2) {
+    const sum = num1 + num2;
+  
+    return sum;
+  }
+  
+  
+  // SUBTRACT
+  
+  
+  function minus(num1, num2) {
+    const minus = num1 - num2;
+  
+    return minus;
+  }
+  
+  
+  // MULTIPLY 
+  
+  
+  function multiply(num1, num2) {
+    const multiply = num1 * num2;
+  
+    return multiply;
+  }
+  
+  
+  // DIVIDE 
+  
+  
+  function divide(num1, num2) {
+    const divide = num1 / num2;
+  
+    return divide;
+  }
