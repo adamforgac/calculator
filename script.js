990f2fbf79cea287fe8b37f8efb65eb5b1aa93c1
Adam Forgac <aforg@email.cz>
2023-01-02 02:03:38 +0100

    Add some important variables
    The most important variables were added to the beginning.

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// IMPORTANT VARIABLES
+// IMPORTANT VARIABLES
+// IMPORTANT VARIABLES
+
+
+
+const numeric = document.querySelectorAll(".numeric");  // ALL NUMBERS
+const operators = document.querySelectorAll(".operator");  // ALL OPERATORS
+const screen = document.querySelector("#onscreen");  // SCREEN
+const allButtons = document.querySelectorAll(".buttons button");  // ALL CALCULATOR BUTTONS
+let currentDisplayValue = "";  // STORES DISPLAY VALUE
+let currentNumber;  // CURRENT SCREEN NUMBER
+let currentOperator;  // CURRENT SCREEN OPERATOR
+let exampleProcess;  // EXAMPLE IN PROCESS
+let example = []; // STORES VALUES FOR EXAMPLES
+const zero = document.querySelector(".zero");  // ZERO BUTTON
+const equal = document.querySelector(".equal");  // EQUAL BUTTON
+const percentage = document.querySelector(".percentage");  // PERCENTAGE BUTTON
+const backSpace = document.querySelector(".backspace"); //   BACKSPACE BUTTON
+const decimal = document.querySelector(".dot");  // DECIMAL BUTTON
+
+
+
 // NUMERICAL OPERATIONS
 // NUMERICAL OPERATIONS
 // NUMERICAL OPERATIONS
@@ -11,27 +34,27 @@ function sum(num1, num2) {
     const sum = num1 + num2;
   
     return sum;
-  }
+}
   
   
   // SUBTRACT
   
   
-  function minus(num1, num2) {
+function minus(num1, num2) {
     const minus = num1 - num2;
   
     return minus;
-  }
+}
   
   
   // MULTIPLY 
   
   
-  function multiply(num1, num2) {
+function multiply(num1, num2) {
     const multiply = num1 * num2;
   
     return multiply;
-  }
+}
   
   
   // DIVIDE 
@@ -41,4 +64,4 @@ function sum(num1, num2) {
     const divide = num1 / num2;
   
     return divide;
-  }
+}
